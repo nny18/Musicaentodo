b240390839ecdfda166789b601728ba351d7b889
nny18 <s.ricoj@uniandes.edu.co>
2025-11-20 18:47:32 +0000

    draft4

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const img = [
-    
+const uno = document.querySelector(".uno");
+const dos = document.querySelector(".dos");
+const tres = document.querySelector(".tres");
+const cuatro = document.querySelector(".cuatro");
+const cinco = document.querySelector(".cinco");
 
-]
+const ventana = document.getElementById("ventana");
+const tituloVentana = document.getElementById("tituloVentana");
 
+uno.addEventListener('click', () => {
+tituloVentana.innerText = "dont smile at"
+
+ventana.style.display = "block";
+
+})
