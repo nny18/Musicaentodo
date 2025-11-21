68a077b503e29d2a7de81f8fc562bc47e68a6cc4
nny18 <s.ricoj@uniandes.edu.co>
2025-11-21 01:10:47 +0000

    draft4.6

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const uno = document.querySelector(".uno");
-const dos = document.querySelector(".dos");
-const tres = document.querySelector(".tres");
-const cuatro = document.querySelector(".cuatro");
-const cinco = document.querySelector(".cinco");
+const uno = document.querySelector("#uno");
+const dos = document.querySelector("#dos");
+const tres = document.querySelector("#tres");
+const cuatro = document.querySelector("#cuatro");
+const cinco = document.querySelector("#cinco");
 
 const ventana = document.getElementById("ventana");
 const tituloVentana = document.getElementById("tituloVentana");
